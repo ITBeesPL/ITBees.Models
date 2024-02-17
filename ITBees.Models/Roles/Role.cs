152b56d0c635b793c434a1aa49f255029916ca87
Jakub Zohar Rutkowski <kuba@skb.pl>
2024-02-17 09:05:08 +0100

    Update available roles

--- ITBees.Models/Roles/Role.cs
+++ ITBees.Models/Roles/Role.cs
@@ -1,6 +1,11 @@
-﻿namespace ITBees.Models.Roles;
-
-public static class Role
+﻿namespace ITBees.Models.Roles
 {
-    public const string PlatformOperator = "PlatformOperator";
+
+    public static class Role
+    {
+        public const string PlatformOperator = "PlatformOperator";
+        public const string PlatformManager = "PlatformManager";
+        public const string PlatformCustomer = "PlatformCustomer";
+        public const string PlatformGuest = "PlatformGuest";
+    }
 }
