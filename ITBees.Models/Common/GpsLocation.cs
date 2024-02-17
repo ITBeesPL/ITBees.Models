a51068bef0bb744d36efb0fbf62227851f77144d
Jakub Zohar Rutkowski <kuba@skb.pl>
2024-02-17 09:20:55 +0100

    Add gps model

---
+++ ITBees.Models/Common/GpsLocation.cs
@@ -0,0 +1,20 @@
+﻿namespace ITBees.Models.Common
+{
+
+    public class GpsLocation
+    {
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+
+        public GpsLocation()
+        {
+
+        }
+
+        public GpsLocation(double latitude, double longitude)
+        {
+            Latitude = latitude;
+            Longitude = longitude;
+        }
+    }
+}
