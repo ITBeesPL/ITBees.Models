af4090802f99b87ff9c92ad02a2a7ae6c7a32bce
Jakub Zohar Rutkowski <kuba@skb.pl>
2024-07-15 10:30:45 +0200

    Expand company model, allow create company without owner, and without created by reference for services that not require login to working

--- ITBees.Models/Companies/Company.cs
+++ ITBees.Models/Companies/Company.cs
@@ -7,11 +7,16 @@ namespace ITBees.Models.Companies
     {
         public Guid Guid { get; set; }
         public string CompanyName { get; set; }
+        public string CompanyShortName { get; set; }
         public DateTime Created { get; set; }
         public UserAccount CreatedBy { get; set; }
-        public Guid CreatedByGuid { get; set; }
+        public Guid? CreatedByGuid { get; set; }
         public UserAccount Owner { get; set; }
-        public Guid OwnerGuid { get; set; }
+        public Guid? OwnerGuid { get; set; }
         public bool IsActive { get; set; }
+        public string Street { get; set; }
+        public string PostCode { get; set; }
+        public string City { get; set; }
+        public string Nip { get; set; }
     }
 }
