7795be2ef41a17f61569123e84390a7f09724993
Jakub Zohar Rutkowski <kuba@skb.pl>
2024-08-09 18:57:40 +0200

    non nullable property TrialNot Available for entity framework

--- ITBees.Models/Companies/CompanyPlatformSubscription.cs
+++ ITBees.Models/Companies/CompanyPlatformSubscription.cs
@@ -9,5 +9,5 @@ public class CompanyPlatformSubscription
     public Guid? SubscriptionPlanGuid { get; set; }
     public string? SubscriptionPlanName { get; set; }
     public DateTime? SubscriptionActiveTo { get; set; }
-    public bool TrialNotAvailable { get; set; }
+    public bool TrialNotAvailable { get; set; } = false;
 }
