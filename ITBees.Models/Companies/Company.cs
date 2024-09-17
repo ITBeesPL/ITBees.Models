f6289aa9e377662ac398d5417678a8c5b0e23958
Jakub Zohar Rutkowski <kuba@skb.pl>
2024-09-17 15:17:08 +0200

    CompanyPlatformSubscriptionPlan navigation property added, and nullable

--- ITBees.Models/Companies/Company.cs
+++ ITBees.Models/Companies/Company.cs
@@ -19,5 +19,6 @@ namespace ITBees.Models.Companies
         public string City { get; set; }
         public string Nip { get; set; }
         public CompanyPlatformSubscription CompanyPlatformSubscription { get; set; }
+        public Guid? CompanyPlatformSubscriptionGuid { get; set; }
     }
 }
