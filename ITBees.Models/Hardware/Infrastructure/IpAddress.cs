d162b125167b6350e9a9c57fbd72b5cd69a13889
Jakub Zohar Rutkowski <kuba@skb.pl>
2024-11-18 08:40:50 +0100

    IpAddress and Ip networks

---
+++ ITBees.Models/Hardware/Infrastructure/IpAddress.cs
@@ -0,0 +1,16 @@
+﻿namespace ITBees.Models.Hardware.Infrastructure;
+
+public class IpAddress
+{
+    public int Id { get; set; }
+    public string Ip { get; set; }
+    public IpNetworkAddress IpNetworkAddress { get; set; }
+    public int IpNetworkAddressId { get; set; }
+    public IpVersionType IpVersion { get; set; }
+}
+
+public enum IpVersionType
+{
+    IPv4,
+    IPv6
+}
---
+++ ITBees.Models/Hardware/Infrastructure/IpNetworkAddress.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using ITBees.Models.Companies;
+using ITBees.Models.Users;
+
+namespace ITBees.Models.Hardware.Infrastructure;
+
+public class IpNetworkAddress
+{
+    public int Id { get; set; }
+    public string NetworkIp { get; set; }
+    public string Netmask { get; set; }
+    public string Gateway { get; set; }
+    public string Dns { get; set; }
+    public Company Company { get; set; }
+    public Guid CompanyGuid { get; set; }
+    public UserAccount CreatedBy { get; set; }
+    public Guid CreatedByGuid { get; set; }
+    public UserAccount UpdatedBy { get; set; }
+    public Guid UpdatedByGuid { get; set; }
+    public bool IsActive { get; set; }
+}
