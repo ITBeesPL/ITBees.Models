adad872cb373690d4ea2b250bd47440bd1a46483
Jakub Zohar Rutkowski <kuba@skb.pl>
2024-11-23 11:30:05 +0100

    Add file media model

---
+++ ITBees.Models/Media/MediaFile.cs
@@ -0,0 +1,23 @@
+﻿using ITBees.Models.Companies;
+using ITBees.Models.Users;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace ITBees.Models.Media;
+
+public class MediaFile
+{
+    [Key]
+    public Guid Guid { get; set; }
+    public string Type { get; set; }
+    public string FileName { get; set; }
+    public string FileExtension { get; set; }
+    public string FilePath { get; set; }
+    public Company Company { get; set; }
+    public Guid? CompanyGuid { get; set; }
+    public UserAccount CreatedBy { get; set; }
+    public Guid CreatedByGuid { get; set; }
+    public DateTime Created { get; set; }
+    public bool IsActive { get; set; }
+    public long FileSize { get; set; }
+}
