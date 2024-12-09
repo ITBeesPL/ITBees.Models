f2052dd3a25d2061fd7503aeacdca4aee194158e
Jakub Zohar Rutkowski <kuba@skb.pl>
2024-12-09 15:51:57 +0100

    Authorized agents changes

---
+++ ITBees.Models/Hardware/RfidReaderDevice.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using ITBees.Models.Buildings;
+using ITBees.Models.Companies;
+using ITBees.Models.Hardware.Infrastructure;
+
+namespace ITBees.Models.Hardware;
+
+public class RfidReaderDevice
+{
+    public Guid Guid { get; set; }
+    public string Ip { get; set; }
+    public string Mac { get; set; }
+    public Company Company { get; set; }
+    public Guid? CompanyGuid { get; set; }
+    public DateTime? LastConnection { get; set; }
+    public string DeviceName { get; set; }
+    public Building Building { get; set; }
+    public Guid? BuildingGuid { get; set; }
+    public IpAddress IpAddress { get; set; }
+    public int? IpAddressId { get; set; }
+    public string TriggerApiEndpoint { get; set; }
+}
