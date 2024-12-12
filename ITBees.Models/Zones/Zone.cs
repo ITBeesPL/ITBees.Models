cc4c98c5c9efb4294c99c1e3b9a36b82b44c3c5e
Jakub Zohar Rutkowski <kuba@skb.pl>
2024-12-12 11:58:44 +0100

    Parking zones extracted

---
+++ ITBees.Models/Zones/Zone.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using ITBees.Models.Users;
+
+namespace ITBees.Models.Zones;
+
+public class Zone
+{
+    public Guid Guid { get; set; }
+    public string ZoneName { get; set; }
+    public UserAccount CreatedBy { get; set; }
+    public DateTime Created { get; set; }
+    public Guid CreatedByGuid { get; set; }
+}
