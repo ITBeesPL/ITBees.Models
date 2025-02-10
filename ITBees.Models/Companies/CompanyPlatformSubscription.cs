9e2ee0f8ebf6e877c5f13179e5422e038b934f48
Jakub Zohar Rutkowski <kuba@skb.pl>
2025-02-10 13:44:51 +0100

    subscription plans - with colors

--- ITBees.Models/Companies/CompanyPlatformSubscription.cs
+++ ITBees.Models/Companies/CompanyPlatformSubscription.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ITBees.Models.Payments;
 using Microsoft.EntityFrameworkCore;
 
 namespace ITBees.Models.Companies;
@@ -7,6 +8,9 @@ namespace ITBees.Models.Companies;
 public class CompanyPlatformSubscription
 {
     public Guid? SubscriptionPlanGuid { get; set; }
+    public PlatformSubscriptionPlan SubscriptionPlan { get; set; }
+    public string? SubscriptionPlanBgColor { get; set; }
+    public string? SubscriptionPlanFgColor { get; set; }
     public string? SubscriptionPlanName { get; set; }
     public DateTime? SubscriptionActiveTo { get; set; }
     public bool TrialNotAvailable { get; set; } = false;
